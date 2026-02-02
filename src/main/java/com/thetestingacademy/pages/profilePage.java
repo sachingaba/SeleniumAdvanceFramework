565b7f5f5c53a80f95596add3c1455d0bc153218
sachin <iamsachingaba@gmail.com>
2026-02-02 19:10:51 +0530

    29th jan

---
+++ src/main/java/com/thetestingacademy/pages/profilePage.java
@@ -0,0 +1,97 @@
+package com.thetestingacademy.pages;
+
+import com.thetestingacademy.utils.PropertiesReader;
+import com.thetestingacademy.utils.WaitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class profilePage {
+    public profilePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    WebDriver driver;
+
+    private By myProfile = By.xpath("//li[@id=\"menuProfile\"]");
+    private By viewProfile = By.id("view_profile");
+    private By firstName = By.name("firstName");
+    private By lastName = By.name("lastName");
+    private By chooseGender = By.xpath("//select[@name=\"gender\"]");
+    private By chooseCountry = By.xpath("//select[@name=\"country\"]");
+    private By chooseState = By.xpath("//select[@name=\"stateId\"]");
+    private By chooseCity = By.xpath("//select[@name=\"cityId\"]");
+    private By dob = By.xpath("//button[@class=\"mat-icon-button\"]");
+    private By openYear = By.xpath("//button[@aria-label=\"Choose month and year\"]");
+    private By chooseYear = By.xpath("//table[@class=\"mat-calendar-table\"]/tbody/tr[1]/td[4]");
+
+    private By chooseMonth = By.xpath("//table[@class=\"mat-calendar-table\"]/tbody/tr[2]/td[2]");
+    private By chooseDate = By.xpath("//table[@class=\"mat-calendar-table\"]/tbody/tr[2]/td[3]");
+    private By submitProfile = By.xpath("//button[@id=\"edit\"]");
+
+    /// /table[@class="mat-calendar-table"]/tbody
+    /// //table[@class="mat-calendar-table"]/tbody/tr/td[@aria-label="1995"]
+    public void updateProfile() {
+        driver.findElement(myProfile).click();
+        driver.findElement(viewProfile).click();
+        driver.findElement(firstName).clear();
+        driver.findElement(firstName).sendKeys(PropertiesReader.readKey("firstname"));
+        driver.findElement(lastName).clear();
+        driver.findElement(lastName).sendKeys(PropertiesReader.readKey("lastname"));
+        //Drop-Downs
+        WebElement Gender = driver.findElement(chooseGender);
+        Select selectGender = new Select(Gender);
+        selectGender.selectByVisibleText(PropertiesReader.readKey("Gender"));
+
+        WebElement Country = driver.findElement(chooseCountry);
+        Select selectCountry = new Select(Country);
+        selectCountry.selectByVisibleText(PropertiesReader.readKey("Country"));
+
+        WebElement State = driver.findElement(chooseState);
+        Select selectState = new Select(State);
+        selectState.selectByVisibleText(PropertiesReader.readKey("State"));
+
+        WebElement City = driver.findElement(chooseCity);
+        Select selectCity = new Select(City);
+        selectCity.selectByVisibleText(PropertiesReader.readKey("City"));
+
+        driver.findElement(dob).click();
+      //  WebElement chooseyearTable = driver.findElement(chooseYear);
+     //   List<WebElement> rows_table = chooseyearTable.findElements(By.tagName("tr"));
+
+        driver.findElement(openYear).click();
+
+WaitHelpers.checkVisibility(driver,chooseYear);
+
+    /*    for (int i = 1; i < rows_table.size(); i++) {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@class=\"mat-calendar-table\"]/tbody/tr[1]/td[4]")));
+            List<WebElement> col_table = rows_table.get(i).findElements(By.tagName("td"));
+            System.out.println("here");
+            for (WebElement col : col_table) {
+
+                if (PropertiesReader.readKey("year").equals(col.getText())) {
+
+                    col.click();
+                    System.out.println(col.getText());
+                    }
+                }*/
+
+
+driver.findElement(chooseYear).click();
+                driver.findElement(chooseMonth).click();
+                driver.findElement(chooseDate).click();
+
+                driver.findElement(submitProfile).click();
+
+
+            }
+
+        }
