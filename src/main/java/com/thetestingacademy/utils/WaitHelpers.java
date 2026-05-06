8d61cd81f692148e30df7b0c4c5d40e7645b9420
SachinGaba <iamsachingaba@gmail.com>
2026-05-06 18:22:24 +0530

    May 6th - 2026

--- src/main/java/com/thetestingacademy/utils/WaitHelpers.java
+++ src/main/java/com/thetestingacademy/utils/WaitHelpers.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.function.Function;
 
@@ -40,6 +41,7 @@ public class WaitHelpers {
     public static void VisibilityOfElement(WebDriver driver, WebElement element){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOf(element));
+        File file =  new File("path","name");
 
     }
     public static WebElement checkVisibilityByFluentWait(WebDriver driver, By locator) {
