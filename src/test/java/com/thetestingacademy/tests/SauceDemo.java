76c7d9e973e9d7a1c69f604d2a4369ec50fd21d9
SachinGaba <iamsachingaba@gmail.com>
2026-06-01 18:18:16 +0530

    May 31st - 2026

--- src/test/java/com/thetestingacademy/tests/SauceDemo.java
+++ src/test/java/com/thetestingacademy/tests/SauceDemo.java
@@ -1,13 +1,16 @@
 package com.thetestingacademy.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SauceDemo {
@@ -17,15 +20,15 @@ public class SauceDemo {
     public void getAllLinks(){
         ChromeOptions options = new ChromeOptions();
 
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("profile.password_manager_enabled", false);
-        prefs.put("safebrowsing.enabled", true);          // keep safe browsing
-        prefs.put("safebrowsing.password_protection_enabled", 0);
-        prefs.put("profile.encrypted_passwords", false);  // depending on the Chrome version may be ignored
-
-        options.setExperimentalOption("prefs", prefs);
-
+//        Map<String, Object> prefs = new HashMap<>();
+//        prefs.put("credentials_enable_service", false);
+//        prefs.put("profile.password_manager_enabled", false);
+//        prefs.put("safebrowsing.enabled", true);          // keep safe browsing
+//        prefs.put("safebrowsing.password_protection_enabled", 0);
+//        prefs.put("profile.encrypted_passwords", false);  // depending on the Chrome version may be ignored
+//
+//        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-features=PasswordManagerUI");
 
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
@@ -41,7 +44,20 @@ public class SauceDemo {
         WebElement log_btn = driver.findElement(By.xpath("//input[@data-test=\"login-button\"]"));
         log_btn.click();
 
-//driver.switchTo().alert().accept();
+driver.navigate().to("https://www.saucedemo.com/inventory-item.html?id=4");
+driver.navigate().to("https://www.saucedemo.com/inventory.html");
+        Actions actions = new Actions(driver);
+
+        actions.sendKeys(Keys.SPACE).build().perform();
+
+       List<WebElement> products = driver.findElements(By.xpath("//div[@data-test=\"inventory-item-price\"]"));
+
+       for(WebElement product: products){
+           product.getText();
+       }
+
+
+
 
 
     }
