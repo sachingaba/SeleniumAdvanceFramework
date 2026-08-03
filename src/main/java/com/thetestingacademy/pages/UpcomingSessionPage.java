586b8bc7481bf7fa2ef6812b19564439e1caa136
SachinGaba <iamsachingaba@gmail.com>
2026-08-03 11:42:22 +0530

    21st June- 2026

--- src/main/java/com/thetestingacademy/pages/UpcomingSessionPage.java
+++ src/main/java/com/thetestingacademy/pages/UpcomingSessionPage.java
@@ -37,6 +37,7 @@ public class UpcomingSessionPage extends CommonToALL {
     //span[@class=\"meta\"]/following-sibling::span
     //div[@class='div-box']//span[contains(text(),"Greenland")]
 
+
     public void viewUpcomingSession() {
         WebDriver driver = DriverManagerTL.getDriver();
         WaitHelpers.checkVisibility(driver, upcomingSession);
